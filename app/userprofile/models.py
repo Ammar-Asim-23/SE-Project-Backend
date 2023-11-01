04105c7b50407fac20affac6c31f76e00b91d74e
Ammar Asim <ammarasim065@gmail.com>
2023-11-01 19:32:43 +0500

    added comments

--- app/userprofile/models.py
+++ app/userprofile/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 
 
 class Userprofile(models.Model):
+    '''
+    Django user class has built in attributes username, first_name, last_name, email, password.
+    Its also has method is_authenticated through which we can check whether the user is logged in.
+    Refer to django documentation for more information https://docs.djangoproject.com/en/4.2/ref/contrib/auth/ 
+    '''
     user = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
     
     
